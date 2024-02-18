5a0ceae4da6d54c3e14e0a8ae20ca34e03bf7cb7
theIntrovertGod <153647452+theIntrovertGod@users.noreply.github.com>
2024-02-18 03:30:45 -0600

    Update main.py
    removed middleware Authorization shitt

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ app = FastAPI()
 
 origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 class model_input(BaseModel):
     
